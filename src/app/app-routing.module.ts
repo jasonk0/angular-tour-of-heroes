4db26cf6dbcd66e82354cb6177ab9bb703df8cf5
Jasonk0 <jasonk0@163.com>
2022-07-05 17:39:23 +0800

    7.5 第三节

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
 
 const routes: Routes = [];
 
